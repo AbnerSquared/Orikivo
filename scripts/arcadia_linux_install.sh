873bff0bf4affaff9678504263f7945cda158998
AbnerSquared <25711694+AbnerSquared@users.noreply.github.com>
2020-12-29 14:41:29 -0600

    Conjoin wget with bash execution

--- scripts/arcadia_linux_install.sh
+++ scripts/arcadia_linux_install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 echo "Installing the latest installer for Arcadia..."
 root=$(pwd)
-wget -N https://raw.githubusercontent.com/AbnerSquared/Orikivo/tree/v1.0.0rc-1/scripts/arcadia_canary_installer.sh
-bash arcadia_canary_installer.sh
+wget -N https://raw.githubusercontent.com/AbnerSquared/Orikivo/tree/v1.0.0rc-1/scripts/arcadia_canary_installer.sh && bash arcadia_canary_installer.sh
 cd "$root"
 rm "$root/arcadia_canary_installer.sh"
 exit 0
