1948d626f1084ff9777c80fba068dc18c0441e15
AbnerSquared <25711694+AbnerSquared@users.noreply.github.com>
2020-12-29 21:10:16 -0600

    Fix minor path errors

--- scripts/arcadia_run.sh
+++ scripts/arcadia_run.sh
@@ -1,14 +1,14 @@
 echo "I have no idea what I'm doing."
 root=$(pwd)
 
-if hash dotnet 2>dev/null
+if hash dotnet 2>/dev/null
 then
 	echo "Found .NET SDK."
 else
 	echo ".NET SDK 3.1 is missing. Please run arcadia_install_preq.sh."
 	exit 1
 fi
-cd $"root/Orikivo"
+cd "$root/Orikivo"
 dotnet restore
 dotnet build -c Release
 cd "$root/Orikivo/Arcadia"
